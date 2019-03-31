79c008379fb1aa23e705443007a0a5d6447be307
harish :) <46072258+HarishKumar-G@users.noreply.github.com>
2019-03-31 21:33:10 +0530

    funny

--- Projects/TickTackToe/TickTackToe.c
+++ Projects/TickTackToe/TickTackToe.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void board();
-int checkwin();
+void board(void);
+int checkwin(void);
 
 char arr[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 void main()
